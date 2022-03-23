c20ff7f8f943d5127dfcd96a9cc4775c16d62f3f
Nguyễn Tiến Thành <tienthanh.jlw@gmail.com>
2022-03-23 23:17:52 +0700

    add exercise 1

---
+++ lesson_1/Exercise_1/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello");
+        int a = 10;
+        int b = 5;
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("b - a = " + (b-a));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("b / a = " + (b/a));
+        int i = 5;
+        System.out.println(i++);
+        System.out.println(++i);
+    }
+}
